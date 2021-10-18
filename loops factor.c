b9843edccd3b6b5789d9e89d523a4a179260d0bd
ThorlapatiSwaroopaRani <92150147+ThorlapatiSwaroopaRani@users.noreply.github.com>
2021-10-18 15:19:10 +0530

    Add files via upload

---
+++ loops factor.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int n=8,i;
+	for(i=1;i<=10;i++)
+	{
+	if(n%i==0)
+	{
+	  printf("factors is:%d\n",i);	
+	}
+	}
+}
---
+++ odd sum loops.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int i,sum=0 ;
+	for(i=1;i<=10;i++)
+	{
+	if(i%2==1)jhope
+	sum=i+sum;
+	}
+	printf("%d\n",sum);
+}
